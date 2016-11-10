b7577912ae40e5ff951b4d221e8004966aa7c9ea
luan on mac pro <vominhluan88@gmail.com>
2016-11-11 00:11:23 +0700

    update

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,30 @@
 import luanvm.vietlott.util.FileReaderUtil;
 
+import java.util.ArrayList;
+
 public class Main {
+
+    private FileReaderUtil fileReader = new FileReaderUtil();
+
+    public void getWorstCases(String key){
+        ArrayList<String> result = fileReader.getWorstFriends("13");
+        System.out.println(result);
+    }
 
     public static void main(String[] args) {
         System.out.println("Ty Phu sau 7h!");
         String inputFile = "src/source/results.txt";
+        Main main = new Main();
+
         FileReaderUtil fileReader = new FileReaderUtil();
         fileReader.readFile(inputFile);
-        fileReader.printAllPairs();
+        //fileReader.printAllPairs();
+        String key = "13";
+        String result = fileReader.getBestFriendOf(key);
+        System.out.println("Best number  of " + key +  "is " + result);
+
+        ArrayList<String> worstFriends = fileReader.getWorstFriends("13");
+        System.out.println("Worst numbers of " + key +" is " + worstFriends);
 
     }
 }
--- src/luanvm/vietlott/util/FileReaderUtil.java
+++ src/luanvm/vietlott/util/FileReaderUtil.java
@@ -9,6 +9,7 @@ import java.util.*;
 public class FileReaderUtil {
 
     TreeMap<String,ArrayList<String>> pairs = new TreeMap<String,ArrayList<String>>();
+    private int startLine = 46;// lan quay thu may
 
     public String readFile(String source){
         String result = "";
@@ -18,11 +19,14 @@ public class FileReaderUtil {
             BufferedReader bufferedReader;
             bufferedReader = new BufferedReader(fileReader);
             String line = bufferedReader.readLine();
+            int count = 1;
             while(line != null){
-              //  System.out.println(line);
+                if(count > startLine)
+                    break;
                 String[] list = line.split(" ");
                 buildPairs(list);
                 line = bufferedReader.readLine();
+                count++;
             }
 
         }catch (FileNotFoundException e){
@@ -79,5 +83,62 @@ public class FileReaderUtil {
             }
             System.out.print(System.lineSeparator());
         }
+    }
+    /*
+        return the best number that have the most appearances with the given number.
+        return 1 con so hay ra nhat voi con so dc chon
+     */
+    public String getBestFriendOf(String key){
+        ArrayList<String> friends = pairs.get(key);
+        ArrayList<String> worstList = getMostFrequents(friends);
+        return worstList.toString();
+    }
+    /*
+        danh sach nhung con so chua bao gio di cung voi key
+     */
+    public ArrayList<String> getWorstFriends(String key){
+        ArrayList<String> result = new ArrayList<String>(45); //maxinum is 45 numbers
+        ArrayList<String> friends = pairs.get(key);
+        for(int i =1; i<=45;i++){
+            String obj = String.valueOf(i);
+            if(i <10)
+                obj = "0" +String.valueOf(i);
+
+            if(!friends.contains(obj)){
+                result.add(obj);
+            }
+        }
+        return result;
+    }
+    private ArrayList<String> getMostFrequents(ArrayList<String> list){
+        ArrayList<String> temp = new ArrayList<String>(list.size());
+        int count=0;
+        TreeMap<String,Integer> frequent = new TreeMap<String,Integer>();
+        for(int i=0; i<list.size();i++){
+            if(frequent.containsKey(list.get(i))){
+                Integer newValue = frequent.get(list.get(i)) + 1;
+                frequent.put(list.get(i), newValue);
+            }else{
+                frequent.put(list.get(i), 1);
+            }
+        }
+        System.out.println(frequent);
+        int max=0;
+        for(int i=0; i<list.size();i++){
+            if(frequent.get(list.get(i)) > max){
+                max = frequent.get(list.get(i));
+            }
+        }
+        count++;
+
+        Set<String> keys = frequent.keySet();
+        Iterator<String> iterator = keys.iterator();
+        while(iterator.hasNext()){
+            String key = iterator.next();
+            if(frequent.get(key) >= max){
+                temp.add(key);
+            }
+        }
+        return temp;
     }
 }
